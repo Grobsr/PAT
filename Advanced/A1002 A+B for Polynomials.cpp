2e34187b9d633dbb43b417f044f1227e749db8c4
冠位观测者 <73771745+Grobsr@users.noreply.github.com>
2021-03-04 16:11:59 +0800

    Update A1002 A+B for Polynomials.cpp

--- Advanced/A1002 A+B for Polynomials.cpp
+++ Advanced/A1002 A+B for Polynomials.cpp
@@ -4,8 +4,8 @@
 using namespace std;
  
 int main(){
-	const int maxNum = 1010;
-    double result[maxNum] = {0};
+    const int maxNum = 1010;
+    double result[maxNum] = {0.0};
     int numOfA, numOfB, exponent;
     double coefficient;
     cin >> numOfA;
@@ -20,16 +20,14 @@ int main(){
         cin >> exponent >> coefficient;
         result[exponent] += coefficient;//把每一项的系数加到结果多项式中
     }
-    int numOfTerms = 0;
     stack<int> out;
-    //遍历数组，让系数不为零的项下标（指数）入栈
+    //遍历结果多项式，让系数不为零的项下标（指数）入栈
     for (int i = 0; i < maxNum; ++i) {
-        if (result[i]){
+        if (result[i] != 0.0){
             out.push(i);
-            numOfTerms++;
         }
     }
-    cout << numOfTerms;//输出多项式的项数
+    cout << out.size();//输出多项式的项数
     while (!out.empty()){
         printf(" %d %.1f", out.top(), result[out.top()]);//输出指数和系数
         out.pop();
